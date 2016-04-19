14d3ac3dc9823b078e543cb9bafc2be4dc5480de
Derek Beck <derek.andrew.beck@gmail.com>
2016-04-19 16:21:36 -0500

    More AI updates

--- Assets/Scripts/Characters/EnemyCharacterController.cs
+++ Assets/Scripts/Characters/EnemyCharacterController.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class EnemyCharacterController : Enemy {
 
     private Inventory _inventory;
+                        protected NavMeshAgent  _navigation;
+    [SerializeField]    protected float         _direction;
+    [SerializeField]    protected float         _chaseDistance;
 
-    [SerializeField] protected float _direction;
-    [SerializeField] protected float _chaseDistance;
 
+    protected NavMeshAgent Navigation {
+        get { return _navigation; }
+        set { _navigation = value; }
+    }
     public float Direction {
         get { return _direction; }
         set { _direction = value; }
@@ -19,18 +24,27 @@ public class EnemyCharacterController : Enemy {
 
     virtual protected void Start() {
         _inventory = GetComponent<Inventory>();
+        Navigation = GetComponent<NavMeshAgent>();
         //Set the default rotation of the Enemy.
         ChangeDirection(Direction);
         InvokeRepeating("CheckDistance", 0.0f, 2.0f);
     }
     virtual protected void OnTriggerEnter(Collider collision) {
-        if(collision.gameObject.CompareTag("Player")) {
-           
+        if(collision.gameObject.CompareTag("Player") && !(State.IsDying || State.IsDead)) {
+            EnemyState _enemyState = (EnemyState)State;
+            _enemyState.ActionState = AIState.Chase;
+            ChangeDirection(0);
+            Navigation.SetDestination(Camera.main.transform.position);
+            _animator.SetBool("IsMoving", true);
         }
     }
     virtual protected void OnTriggerStay(Collider collision) {
-        if (collision.gameObject.CompareTag("Player")) {
-
+        if (collision.gameObject.CompareTag("Player") && !(State.IsDying || State.IsDead)) {
+            EnemyState _enemyState = (EnemyState)State;
+            _enemyState.ActionState = AIState.Chase;
+            ChangeDirection(0);
+            Navigation.SetDestination(Camera.main.transform.position);
+            _animator.SetBool("IsMoving", true);
         }
     }
 
@@ -42,11 +56,12 @@ public class EnemyCharacterController : Enemy {
     virtual protected void CheckDistance() {
         float distance = Vector3.Distance(gameObject.transform.position, Camera.main.transform.position);
         if (distance <= ChaseDistance) {
-            EnemyState _enemyState = (EnemyState)State;
-            _enemyState.ActionState = AIState.Chase;
+
         }
         else if(distance <= _inventory.Weapon.Range) {
             //Set state to attack.
+            Navigation.Stop();
+            Attack();
         }
         else {
             //Set state to patrol.
